94480bb88c108ace5cc762906bb00304cb2a24ff
TJ Hutchinson <tjhutch92@gmail.com>
2020-12-22 12:17:57 -0800

    Picture corrections and lives banner

---
+++ script.js
@@ -0,0 +1,11 @@
+let numberEl = document.querySelectorAll("#lives-saved-banner-number");
+var incrementEl = document.querySelector("#increment");
+
+let  livesSaved = [""];
+let numberOfLivesSaved = livesSaved.length;
+let count = 0;
+
+
+// $("lives-saved-banner-number").append(numberOfLivesSaved); 
+
+$("#lives-saved-banner-number").text(count);
